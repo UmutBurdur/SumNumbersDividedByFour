9f4a990b7aa3315706dc64dff90087c3e376fc4f
Umut Burdur <euburdur28@gmail.com>
2021-07-11 22:58:26 +0300

    Second upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SumNumbersDividedByFour
+
+
+This is a homework at [Patika.dev](https://app.patika.dev/egitimler/java-ile-backend-web-development-patikasi/java101/pratik-tek-sayi-toplam)
+
 This program will sum the input numbers which can be divided by 4 until odd number is given as an input.
--- sumNumbersDividedByFour/src/main/Main.java
+++ sumNumbersDividedByFour/src/main/Main.java
@@ -1,2 +1,28 @@
+package main;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int number;
+        int total=0;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.print("Please give me a number: ");
+            number = scanner.nextInt();
+            if (number % 4 == 0) {
+                total += number;
+                System.out.println("Summation of numbers which can be divided by four for now: "
+                                + total);
+            }
+            if (number % 4 != 0 && number % 2 == 0) {
+                System.out.println("Summation of numbers which can be divided by four is not changed");
+            }
+            if (number % 2 != 0) {
+                System.out.println("Please give an even number as an input next time");
+            }
+
+        } while (number % 2 == 0);
+    }
 }
